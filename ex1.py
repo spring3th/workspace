b4a150363a76c1f6f25fc169a3bebf048f260057
springdeng <dengchangqing@gmail.com>
2013-12-09 22:14:57 +0800

    first commit 1209

---
+++ ex1.py
@@ -0,0 +1,30 @@
+#First Programe
+print("Hello World!")
+print("I like typing this.")
+
+print("--------------------")
+print("I will now count my chickens:")
+
+print("Hens",25+30/6)
+
+my_name = 'Zed A. Shaw'
+my_age = 35 #not a lie
+my_height = 74 #inches
+my_weight = 180 #1bs
+my_eyes = 'Blue'
+my_teeth = 'White'
+my_hair = 'Brown'
+
+print("Let's talk about %s." % my_name)
+print("He's %d inches tall." % my_height)
+print("He's %d pounds heavy." % my_weight)
+print("Actually that's not too heavy.")
+
+x = y = [1,2,3]
+z = [1,2,3]
+print(x == y)
+
+print(x is y)
+print(x is z)
+
+
