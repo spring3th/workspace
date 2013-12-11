a52ecf4d585fbe3abaf535c7e30e6ab52ecdbd36
springdeng <dengchangqing@gmail.com>
2013-12-11 17:43:46 +0800

    step two

---
+++ ex2.py
@@ -0,0 +1,10 @@
+words = ['this','is','an','ex','parrot']
+for word in words:
+    print(word)
+numbers = [1,2,3,4,5,6,7,8,9]
+for number in numbers:
+    print number
+print(range(10))
+
+for number in range(1,10):
+    print number
