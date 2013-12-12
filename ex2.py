e83a5b63b72c5b75aebaf9205a17a0ceacb19345
springdeng <dengchangqing@gmail.com>
2013-12-12 23:36:16 +0800

    1212

--- ex2.py
+++ ex2.py
@@ -8,3 +8,19 @@ print(range(10))
 
 for number in range(1,10):
     print number
+
+d = {'x':1,'y':2,'z':3}
+for key in d:
+    print(key , 'corresponds to',d[key])
+
+names = ['name','beth','george','damon']
+ages = [12,45,32,102]
+
+print(zip(names,ages))
+for name,age in zip(names,ages):
+    print name,'is',age,'years old'
+
+for i in range(len(names)):
+    print(names[i],'is',ages[i],'years old')
+
+
