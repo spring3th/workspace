4b4f12984a7b7874d2bd25f95b4b0929d05eb900
springdeng <dengchangqing@gmail.com>
2013-12-13 22:32:09 +0800

    add level

--- third.py
+++ third.py
@@ -1,11 +1,13 @@
 movies = ["taitannikehao",1969,"sejie",2010,
            ["The mean of life",2012,"spring","yini"]]
 
-def print_lol(the_list):
+def print_lol(the_list,level):
     for the_item in the_list:
         if isinstance(the_item,list):
-           print_lol(the_item)
+           print_lol(the_item,level+1)
         else:
+           for tab_stop in range(level):
+               print("\t")
            print(the_item)
 
-print_lol(movies)
+print_lol(movies,2)
