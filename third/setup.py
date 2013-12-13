97502a923cf8ae748ddf0ef5232fde2f4a7e2451
springdeng <dengchangqing@gmail.com>
2013-12-13 21:12:31 +0800

    setup module

---
+++ third/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+        name = 'third',
+        version  = '1.0.0',
+        py_modules = ['third'],
+        author = 'springdeng',
+        author_email = 'dengchangqing@gmail.com',
+        url = 'http://www.ssjike.com',
+        description = 'A simple print of nested lists',
+     ) 
