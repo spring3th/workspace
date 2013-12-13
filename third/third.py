356c5f7df8da5eb721df42f4b82cd880ecc95841
springdeng <dengchangqing@gmail.com>
2013-12-13 21:29:04 +0800

    modify

--- third/third.py
+++ third/third.py
@@ -1,3 +1,5 @@
+#author :springdeng
+"""递归输出列表，建立函数递归查找，打印输出"""
 movies = ["taitannikehao",1969,"sejie",2010,
            ["The mean of life",2012,"spring","yini"]]
 
@@ -7,5 +9,5 @@ def print_lol(the_list):
            print_lol(the_item)
         else:
            print(the_item)
-
+#调用函数，传递参数
 print_lol(movies)
