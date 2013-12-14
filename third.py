11ce185a9a3005ef108f3a44a529efc9dfcd9b23
springdeng <dengchangqing@gmail.com>
2013-12-14 17:58:06 +0800

    file io read

--- third.py
+++ third.py
@@ -1,6 +1,12 @@
+"""
+author:spring
+author_email:dengchagnqing@gmail.com
+sit:www.ssjike.com
+"""
+#list opration
 movies = ["taitannikehao",1969,"sejie",2010,
            ["The mean of life",2012,"spring","yini"]]
-
+#genju tiaojian lai xunfan bianli list
 def print_lol(the_list,indent=False,level=0):
 	for the_item in the_list:
 		if isinstance(the_item,list):
@@ -12,3 +18,22 @@ def print_lol(the_list,indent=False,level=0):
 			print(the_item)
 
 print_lol(movies,True,2)
+
+#==========================
+import os
+print(os.getcwd())#fan hui dangqian mulu
+#qie huan dao zhiding mulu 
+#os.chdir('../home/software')
+
+data = open('test.txt')
+
+data.seek(0)#wenjian qishi
+for each_line in data:
+	(role,line_spoken) = each_line.split(':',1)
+	print(role,end='')
+	print('said:',end='')
+	print(line_spoken,en='')
+data.close()
+#print(data.readline(),end='')
+
+
