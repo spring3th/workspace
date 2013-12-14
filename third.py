647b6f12cd80e71dcb69c2ae27c09a981ef3ade2
springdeng <dengchangqing@gmail.com>
2013-12-14 15:12:45 +0800

    add Fale tab

--- third.py
+++ third.py
@@ -1,13 +1,14 @@
 movies = ["taitannikehao",1969,"sejie",2010,
            ["The mean of life",2012,"spring","yini"]]
 
-def print_lol(the_list,level):
-    for the_item in the_list:
-        if isinstance(the_item,list):
-           print_lol(the_item,level+1)
-        else:
-           for tab_stop in range(level):
-               print("\t")
-           print(the_item)
+def print_lol(the_list,indent=False,level=0):
+	for the_item in the_list:
+		if isinstance(the_item,list):
+			print_lol(the_item,indent,level+1)
+		else:
+			if indent:
+				for tab_stop in range(level):
+					print("\t",end='')
+			print(the_item)
 
-print_lol(movies,2)
+print_lol(movies,True,2)
