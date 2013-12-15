29d0e04df32b653af30ae8df1d4dac74e5059098
springdeng <dengchangqing@gmail.com>
2013-12-15 19:29:08 +0800

    pickle write load

--- springDemo/src/four.py
+++ springDemo/src/four.py
@@ -1,3 +1,5 @@
+import pickle
+
 man = []
 other = []
 try:
@@ -37,5 +39,28 @@ try:
         print(other,file=other_file)
 except IOError as err:
         print('File error:'+str(err))
+        
+#=============================================
+
+
+try:
+    with open('man_file.txt','wb') as man_file,open('other_file.txt','wb') as other_file:
+        pickle.dump(man, man_file)
+        pickle.dump(other, other_file)
+except pickle.PickleError as perr:
+    print('pickling error:'+str(perr))
+    
+new_man = []
+new_other = []
+try:
+    with open('man_file.txt','rb') as man_file,open('other_file.txt','rb') as other_file:
+        new_man = pickle.load(man_file)
+        new_other = pickle.load(other_file)
+        print(new_man)
+        print(new_other)
+except IOError as err:
+    print('File error:'+str(err))
+except pickle.PickleError as perr:
+    print('pickling error:'+str(perr))
 
                 
