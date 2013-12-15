d48c1f967c4269d41634f97f31dd477e7771f061
springdeng <dengchangqing@gmail.com>
2013-12-15 20:54:09 +0800

    modify function

--- springDemo/src/four.py
+++ springDemo/src/four.py
@@ -44,7 +44,7 @@ except IOError as err:
 
 
 try:
-    with open('man_file.txt','wb') as man_file,open('other_file.txt','wb') as other_file:
+    with open('man_file.txt','wb') as man_file,open('other_file.txt','wb') as other_file:#wb是以二进制的形式写入文件
         pickle.dump(man, man_file)
         pickle.dump(other, other_file)
 except pickle.PickleError as perr:
@@ -53,7 +53,7 @@ except pickle.PickleError as perr:
 new_man = []
 new_other = []
 try:
-    with open('man_file.txt','rb') as man_file,open('other_file.txt','rb') as other_file:
+    with open('man_file.txt','rb') as man_file,open('other_file.txt','rb') as other_file:#以二进制的形式读取文件
         new_man = pickle.load(man_file)
         new_other = pickle.load(other_file)
         print(new_man)
@@ -63,4 +63,27 @@ except IOError as err:
 except pickle.PickleError as perr:
     print('pickling error:'+str(perr))
 
+"""将不同方式表示的时间间隔进行统一处理成点号分割的时间"""
+def sanitize(time_string):
+    if '-' in time_string:#-是否包含在字符串
+        splitter = '-'
+    elif ':' in time_string:#：是否包含在字符串
+        splitter = ':'
+    else:
+        return(time_string)
+    (mins,secs) = time_string.split(splitter)
+    return(mins+'.'+secs)
+
+with open('james.txt') as jaf:
+    data = jaf.readline()
+    james = data.strip().split(',')
+print(james)
+print(sorted(james))
+
+clean_james = []
+#迭代处理每个以逗号分割的时间，按格式化成点号分割。
+for each_t in james:
+    clean_james.append(sanitize(each_t))
+print(sorted(clean_james))
+                        
                 
